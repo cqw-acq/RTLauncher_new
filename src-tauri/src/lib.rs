1ea8dad202b30ef1f0e2710e1c3e48f4909570ec
ElandaDRM <89924166+ElandaDRM@users.noreply.github.com>
2026-03-03 10:02:54 +0800

    Update lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,13 @@
+mod handler;
+mod downloader;
+use handler::launcher::build_jvm_arguments;
+use downloader::original_dwl::download_task;
+use downloader::version_fetcher::classify_minecraft_versions;
+use downloader::decompression::extract_library_paths;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![build_jvm_arguments,:download_task,classify_minecraft_versions,extract_library_paths])
     .setup(|app| {
       if cfg!(debug_assertions) {
         app.handle().plugin(
